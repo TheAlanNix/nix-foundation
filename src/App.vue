c8950977b98daee31ff106f12720b28fc512e4b8
Alan Nix <alanneilnix@gmail.com>
2023-04-13 10:22:11 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img id="portrait" alt="Portrait" src="./assets/portrait.jpg" />
+  <NixFoundation msg="Welcome to the Lynn Nix Foundation" />
+</template>
+
+<script>
+import NixFoundation from "./components/NixFoundation.vue";
+
+export default {
+  name: "App",
+  components: {
+    NixFoundation,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#portrait {
+  border-radius: 25px;
+  filter: drop-shadow(0 0 0.2rem gray);
+}
+</style>
---
+++ src/components/NixFoundation.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      Honoring the lifelong goal of Carolyn "Lynn" Nix in which every one of us
+      can read and write,<br />
+      and there is free access to education for all.
+    </p>
+    <h3>Essential Links</h3>
+    <ul>
+      <li>
+        <a href="#" rel="noopener">See How</a>
+      </li>
+      <li>
+        <a href="#" rel="noopener">Support Us</a>
+      </li>
+      <li>
+        <a href="#" rel="noopener">Forum</a>
+      </li>
+      <li>
+        <a href="#" rel="noopener">News</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/wiz_io" target="_blank" rel="noopener">Twitter</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NixFoundation",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
