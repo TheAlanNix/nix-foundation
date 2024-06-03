e73d178d8005cefdd1ebf8c85a12d5d0f1259b5f
Alan Nix <alanneilnix@gmail.com>
2024-06-03 10:36:08 -0400

    revert foundation name

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img id="portrait" alt="Portrait" src="./assets/portrait.jpg" />
-  <NixFoundation msg="Welcome to the Carolyn Nix Foundation" />
+  <NixFoundation msg="Welcome to the Lynn Nix Foundation" />
 </template>
 
 <script>
