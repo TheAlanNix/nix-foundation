9b9bc7cef720ec6dc75eca644eb10f56c5521bcd
Justin Quinn <justinquinn51691@gmail.com>
2024-06-29 07:58:08 -0400

    Changed foundation name again

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img id="portrait" alt="Portrait" src="./assets/portrait.jpg" />
-  <NixFoundation msg="Welcome to the Justin Quinn Foundation" />
+  <NixFoundation msg="Welcome to the Dick Umile Foundation" />
 </template>
 
 <script>
