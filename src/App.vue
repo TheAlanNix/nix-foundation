98c8468c54fd7df0cce9824d18fe6f29573117b4
Justin Quinn <justinquinn51691@gmail.com>
2024-06-30 09:01:01 -0400

    Updated name to Justin Test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img id="portrait" alt="Portrait" src="./assets/portrait.jpg" />
-  <NixFoundation msg="Welcome to the Wiz Foundation" />
+  <NixFoundation msg="Welcome to the Justin Test Foundation" />
 </template>
 
 <script>
