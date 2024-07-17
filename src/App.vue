6dbd0b46f045c649181ac2f6bb7f445b6f21c4a6
Justin Quinn <justinquinn51691@gmail.com>
2024-07-17 07:40:37 -0400

    updated site name

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img id="portrait" alt="Portrait" src="./assets/portrait.jpg" />
-  <NixFoundation msg="Welcome to The Mighty Quinn Music"/>
+  <NixFoundation msg="Welcome to The Mighty Quinn Music (Not the Manfred Mann song)"/>
 </template>
 
 <script>
